6b7407713b506c05667b7edd489fc6230e295ce4
invisibletr4ve1er <99118231+invisibletr4ve1er@users.noreply.github.com>
2023-04-07 22:38:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 124625853 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+
+    x_bar = np.mean(x)
+
+    s = np.std(x, ddof=1)
+
+    df = n - 1
+
+    lower_bound = (n-1)*s**2/chi2.ppf(1-alpha/2,df)
+    upper_bound = (n-1)*s**2/chi2.ppf(alpha/2, df)
+
+    # Return the result as a tuple
+    return (np.sqrt(lower_bound/39), np.sqrt(upper_bound/39))
